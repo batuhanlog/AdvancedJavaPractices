30e02c85b48b61782fa782eb268bd91993d2e488
batuhanlog <mrkayahan66@gmail.com>
2023-12-14 23:13:21 +0300

    veterinary Management

---
+++ Hafta-12/spring/spring/src/main/java/dev/patika/spring/Customer.java
@@ -0,0 +1,2 @@
+package dev.patika.spring;public class Customer {
+}
--- Pratikler/src/OrderNameCompare.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class OrderNameCompare implements Comparator<Student> {
-
-    @Override
-    public int compare(Student o1, Student o2) {
-        return o1.getName().compareTo(o2.getName());
-    }
-}
